3d1497f7354c20366e088038ca80f661d628c4e7
Developrimbor <bymrglmz@gmail.com>
2025-08-28 21:07:09 +0300

    PageTransition bileşeni layout.tsx dosyasına eklendi. Bu değişiklik, sayfa geçişlerini iyileştirmek ve kullanıcı deneyimini artırmak amacıyla yapıldı.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import PageTransition from "@/components/ui/PageTransition";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${roboto.variable} ${geistMono.variable} antialiased`}
       >
+        <PageTransition />
         <Header />
         <main className="pt-16">
           {children}
