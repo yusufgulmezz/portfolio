2c56cccdd6f0e38d91f007da7a895eaf4eeb0455
Developrimbor <bymrglmz@gmail.com>
2025-08-31 20:42:59 +0300

    PageTransition bileşeni Home sayfasına eklendi. Bu değişiklik, sayfa geçişleri sırasında kullanıcı deneyimini geliştirmek amacıyla yapıldı.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/sections/HeroSection";
 import CategoriesSection from "@/components/sections/CategoriesSection";
+import PageTransition from "@/components/ui/PageTransition";
 
 export default function Home() {
   return (
     <>
+      <PageTransition />
       <HeroSection />
       <CategoriesSection />
     </>
