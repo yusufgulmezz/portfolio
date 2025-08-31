3f497a83ec6528a4aa0795ba3df1471a9949f803
Developrimbor <bymrglmz@gmail.com>
2025-08-31 20:43:15 +0300

    layout.tsx dosyasında dil ayarı İngilizce olarak değiştirildi. PageTransition bileşeni kaldırıldı ve ana içerik alanının üst boşluğu artırıldı.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Roboto, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import PageTransition from "@/components/ui/PageTransition";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -27,13 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="tr">
+    <html lang="en">
       <body
         className={`${roboto.variable} ${geistMono.variable} antialiased`}
       >
-        <PageTransition />
         <Header />
-        <main className="pt-16">
+        <main className="pt-20">
           {children}
         </main>
         <Footer />
