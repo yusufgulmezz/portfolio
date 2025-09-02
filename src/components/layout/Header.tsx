f1d47ccd289ab78a95303aa3b30f205a0042e9cd
Developrimbor <bymrglmz@gmail.com>
2025-09-03 00:23:28 +0300

    Header bileşeninde, orta ekranlar için yükseklik 20 pikselden 24 piksele çıkarıldı. Bu değişiklik, tasarımın uyumunu artırmak amacıyla yapıldı.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
       className="fixed top-0 left-0 right-0 z-50 backdrop-blur-xl"
     >
              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-         <div className="flex justify-between items-center h-16 sm:h-20">
+         <div className="flex justify-between items-center h-16 sm:h-24">
           {/* Left Side - Logo + rotating subtitle */}
           <div className="flex-shrink-0">
             <button 
