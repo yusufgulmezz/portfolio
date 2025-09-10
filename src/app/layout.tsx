dbf817c395b754d5cd4b95ab6847a6b9627af121
Developrimbor <bymrglmz@gmail.com>
2025-09-10 19:00:49 +0300

    BlobCursor bileşeni layout'a eklendi; kullanıcı deneyimini geliştirmek için etkileşimli bir imleç efekti sağlandı.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import VerticalNav from "@/components/layout/VerticalNav";
+import BlobCursor from "@/components/ui/BlobCursor";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -31,6 +32,24 @@ export default function RootLayout({
       <body
         className={`${roboto.variable} ${geistMono.variable} antialiased`}
       >
+        <BlobCursor
+          blobType="circle"
+          fillColor="#5227FF"
+          trailCount={3}
+          sizes={[28, 56, 18]}
+          innerSizes={[10, 18]}
+          innerColor="rgba(255,255,255,0.8)"
+          opacities={[0.4, 0.4]}
+          shadowColor="rgba(0,0,0,0.4)"
+          shadowBlur={3}
+          shadowOffsetX={8}
+          shadowOffsetY={8}
+          filterStdDeviation={20}
+          useFilter={true}
+          fastDuration={0.1}
+          slowDuration={0.5}
+          zIndex={100}
+        />
         <VerticalNav />
         <Header />
         <main className="pt-20 sm:pt-24 lg:pt-18">
