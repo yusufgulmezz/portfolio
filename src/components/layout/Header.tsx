13dc2e193fcbbd461f1bb9a47a3e69d54ba49d0a
Developrimbor <bymrglmz@gmail.com>
2025-11-21 18:36:18 +0300

    Adjust Header component height for improved layout consistency

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
       className="fixed top-0 left-0 right-0 z-50 backdrop-blur-xl"
     >
              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-         <div className="relative flex justify-between items-center h-16 sm:h-24">
+         <div className="relative flex justify-between items-center h-14 sm:h-20">
           {/* Left Side - Logo + rotating subtitle */}
           <div className="flex-shrink-0">
             <button 
